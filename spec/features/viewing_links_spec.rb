5e669361eda512788c7da902b57b8713117abc2d
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-14 18:11:33 +0000

    renamed app spec to viewing links spec

---
+++ spec/features/viewing_links_spec.rb
@@ -0,0 +1,15 @@
+require 'capybara/rspec'
+require './app/models/link.rb'
+
+feature "List of links on homepage" do
+  scenario 'When i\'m visiting a home page I get a list of my links ' do
+      Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+      visit('/links')
+      # this is a *temporary* sanity check - to make sure we
+      # can load the page
+      expect(page.status_code).to eq 200
+      within 'ul#links' do
+      expect(page).to have_content "Makers Academy"
+    end
+  end
+end
