58efb18f5ee358227d0a116afeb60d304d85d5b1
Oleg Grushetskyy <olegfkl@gmail.com>
2017-11-15 17:24:06 +0000

    Update Gemfile

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@
 require 'dm-postgres-adapter'
 require 'data_mapper'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
 class Link
   include DataMapper::Resource
