0c961cb6229ccbd9c89fc4d72f4c0ec46f6a7598
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-16 16:37:47 +0000

    adds User model

--- app/models/database_config.rb
+++ app/models/database_config.rb
@@ -1,7 +1,10 @@
 require_relative 'link'
 require_relative 'tag'
+require_relative 'user'
+
 require 'dm-postgres-adapter'
 require 'data_mapper'
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
