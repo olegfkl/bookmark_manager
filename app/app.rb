a124026e5b62489be46c8e50d8e9c42ea621d441
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-16 16:38:39 +0000

    adds signup feature

--- app/app.rb
+++ app/app.rb
@@ -3,37 +3,50 @@ ENV['RACK_ENV'] ||= 'development'
 require './app/models/database_config'
 require 'sinatra/base'
 require './app/models/link.rb'
+require './app/models/user.rb'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
 
-get '/' do
-  # @link1 = BookmarkManager.get(1)
-  erb(:index)
-end
-
-get '/links' do
-  @links = Link.all
-  erb(:links)
-end
-
-get '/links/new' do
-  erb(:new)
-end
-
-get '/tags/:tag' do |name|
-  tag = Tag.first(name: name)
-  @links = tag ? tag.links : []
-  erb(:links)
-end
-
-post '/links/save' do
-  link = Link.create(title: params[:title], url: params[:url])
-  params[:tag].split(',').map(&:strip).each do |tag|
-    link.tags << Tag.first_or_create(name: tag)
+  get '/' do
+    # @link1 = BookmarkManager.get(1)
+    erb(:index)
   end
-  link.save
-  redirect '/links'
-end
 
-run! if app_file == $0
+  get '/signup' do
+    erb(:signup)
+  end
+
+  post '/new-user' do
+    User.create(
+      username: params[:username], password: params[:password])
+    session[:username] = params[:username]
+    redirect '/links'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb(:links, locals: { username: session[:username] })
+  end
+
+  get '/links/new' do
+    erb(:new)
+  end
+
+  get '/tags/:tag' do |name|
+    tag = Tag.first(name: name)
+    @links = tag ? tag.links : []
+    erb(:links)
+  end
+
+  post '/links/save' do
+    link = Link.create(title: params[:title], url: params[:url])
+    params[:tag].split(',').map(&:strip).each do |tag|
+      link.tags << Tag.first_or_create(name: tag)
+    end
+    link.save
+    redirect '/links'
+  end
+
+  run! if app_file == $0
 end
